9b061ac07907aa05551c2343f52bbebb3afb911b
Brenda <62002152+Brenda0714@users.noreply.github.com>
2024-03-25 18:36:50 -0600

    Tips al finalizar las preguntas

--- src/app/components/recommendations/recommendations.component.ts
+++ src/app/components/recommendations/recommendations.component.ts
@@ -30,7 +30,7 @@ export class RecommendationsComponent implements OnInit {
   clientName: string | null = '';
   recommendedProducts: Array<any> = [];
 
-  numberProducts: number = 6;
+  numberProducts: number = 4;
 
   values = [];
 
